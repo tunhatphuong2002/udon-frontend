96ac42fd5f7733a587ccc21b56201a79592ac8f2
nathanielfsn <phuongtn@mirailabs.co>
2025-04-19 16:16:49 +0700

    feat(config): update Next.js configuration and modify image handling in Hero component

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
