48f2d509c13bf96894b45b914f2a6c6f158f45f7
nathanielfsn <phuongtn@mirailabs.co>
2025-06-06 10:04:37 +0700

    fix: error from vercel

--- src/app/(protected)/reserve/components/risk.tsx
+++ src/app/(protected)/reserve/components/risk.tsx
@@ -5,25 +5,14 @@ export const VaultRisk: React.FC = () => {
   return (
     <div className="w-full mt-6 max-md:max-w-full">
       <div className="flex w-full items-center gap-[18px] flex-wrap max-md:max-w-full">
-        <StatCard
-          title="Risk Rating"
-          value={
-            <>
-              <span className="font-medium text-base">Not provided by Curator</span>
-            </>
-          }
-        />
-        <StatCard title="Curator TVL" value="$244.45M" />
-        <StatCard title="Vault Deployment Date" value="24/07/2024" />
+        <StatCard title="Risk Rating" value={2} />
+        <StatCard title="Curator TVL" value={244450000} />
+        <StatCard title="Vault Deployment Date" value={24072024} />
       </div>
       <div className="flex w-full items-stretch gap-4 flex-wrap mt-4 max-md:max-w-full">
-        <StatCard title="Owner" value="0xD4C8...2446" icon="/images/tokens/usdt.png" />
-        <StatCard title="Curator Address" value="0xD4C8...2446" icon="/images/tokens/usdt.png" />
-        <StatCard
-          title="Timelock / Guardian"
-          value="0xD4C8...2446"
-          icon="/images/tokens/usdt.png"
-        />
+        <StatCard title="Owner" value={12} icon="/images/tokens/usdt.png" />
+        <StatCard title="Curator Address" value={12} icon="/images/tokens/usdt.png" />
+        <StatCard title="Timelock / Guardian" value={12} icon="/images/tokens/usdt.png" />
       </div>
     </div>
   );
